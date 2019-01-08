370b156add0585bf9134bd9adcb6b40e4b8c8673
Martin Graversen <martin@graversen.io>
2019-01-08 12:05:00 +0100

    :bulb: + ApiVisibility.RESTRICTED

--- jsondoc-core/src/main/java/com/axonmobileiot/jsondoc/core/pojo/ApiVisibility.java
+++ jsondoc-core/src/main/java/com/axonmobileiot/jsondoc/core/pojo/ApiVisibility.java
@@ -1,15 +1,30 @@
 package com.axonmobileiot.jsondoc.core.pojo;
 
 public enum ApiVisibility {
-	UNDEFINED(""), PRIVATE("PRIVATE"), PUBLIC("PUBLIC");
+    UNDEFINED(""),
 
-	private String label;
+    /**
+     * Indicates that the API is not for public consumption.
+     */
+    PRIVATE("PRIVATE"),
 
-	ApiVisibility(String label) {
-		this.label = label;
-	}
+    /**
+     * Indicates that the API is freely available (general authentication / authorization schemes apply).
+     */
+    PUBLIC("PUBLIC"),
 
-	public String getLabel() {
-		return label;
-	}
+    /**
+     * Indicates that the API requires special privilege to access.
+     */
+    RESTRICTED("RESTRICTED");
+
+    private String label;
+
+    ApiVisibility(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
